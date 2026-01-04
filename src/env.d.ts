4a86b446ff66b7b61ff5963cefaea874d037488a
LeszczynskiKarol <leszczu123@wp.pl>
2026-01-04 11:28:52 +0100

    przed deployem

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
+
+declare function gtag(...args: any[]): void;
+
+declare function openCookieSettings(): void;
+
+interface Window {
+  dataLayer: any[];
+  gtag: typeof gtag;
+  openCookieSettings: typeof openCookieSettings;
+}
