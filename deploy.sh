f6de7a03480c5363f931299277dc5fa9949f9d19
LeszczynskiKarol <leszczu123@wp.pl>
2026-03-26 11:24:03 +0100

    git push from local

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,17 @@ S3_BUCKET="www.prace-magisterskie.pl"
 CLOUDFRONT_ID="E42YH6IKMW3J7"  # <-- To jest poprawne ID dystrybucji www
 
 cd /d/prace-magisterskie.pl
+
+echo "📦 Pushing to GitHub..."
+git add .
+git commit -m "git push from local"
+git push origin main
+
+if [ $? -ne 0 ]; then
+  echo "❌ Git push failed!"
+  exit 1
+fi
+
 npm run build
 aws s3 sync dist/ s3://${S3_BUCKET} --delete
 aws cloudfront create-invalidation --distribution-id ${CLOUDFRONT_ID} --paths "/*"
